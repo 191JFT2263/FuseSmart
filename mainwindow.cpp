000da90143ac1723c467cf0ac0239e576daa68d2
oussama120 <67621280+oussama120@users.noreply.github.com>
2020-11-11 02:07:28 +0100

    Add files via upload

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,47 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include "contrat.h"
-#include "dialog.h"
-
-
+#include <QMessageBox>
+#include <QDebug>
+#include <QSound>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    ui->lineEdit_p->setValidator(new QIntValidator(0,99999999,this));
+    ui->stackedWidget->setCurrentIndex(0);// initialiser le premier widget
+    //animation pour logo fusesmart
+    int w=ui->logo->width();
+    int h=ui->logo->height();
+    animation = new QPropertyAnimation(ui->logo,"geometry");
+    animation->setDuration(10000);
+    animation->setStartValue(ui->logo->geometry());
+    animation->setEndValue(QRect(0,20,w,h));
+
+    QEasingCurve curve;
+    curve.setType(QEasingCurve::OutBounce);
+    animation->setEasingCurve(curve);
+    //curve.setAmplitude(2.00);
+    curve.setOvershoot(1.30);
+    curve.setPeriod(0.50);
+    //animation->setLoopCount(3);
+
+    animation->start();
+ //animation pour logo sofap
+
+    int w1=ui->label_7->width();
+    int h2=ui->label_7->height();
+    animation1 = new QPropertyAnimation(ui->label_7,"geometry");
+    animation1->setDuration(10000);
+    animation1->setStartValue(ui->label_7->geometry());
+    animation1->setEndValue(QRect(500,200,w1,h2));
+    QEasingCurve curve2;
+    curve2.setType(QEasingCurve::OutBounce);
+    animation1->setEasingCurve(curve2);
+    curve2.setOvershoot(1.30);
+    curve2.setPeriod(0.50);
+    animation1->setLoopCount(-1);
+
+      animation1->start();
 }
 
 MainWindow::~MainWindow()
@@ -18,17 +50,124 @@ MainWindow::~MainWindow()
 }
 
 
-void MainWindow::on_pushButton_clicked()
+void MainWindow::on_pushButton_clicked() // boutton ajouter materiaux
 {
-     Contrat c;
-     c.setnum(ui->lineEdit_num->text());
-     c.setdate(ui->date->date());
-     c.setprixelement(ui->lineEdit_p->text().toFloat());
-     c.setqte(ui->lineEdit_qte->text().toInt());
+     ui->stackedWidget->setCurrentIndex(1);
+}
 
-     Dialog d;
-     d.setcontact(c);
-     d.exec();
+void MainWindow::on_pushButton_2_clicked() // boutton afficher materiaux
+
+{ui->stackedWidget->setCurrentIndex(2);
 
 }
 
+void MainWindow::on_modifier_2_clicked() // boutton modifier
+{
+    ui->stackedWidget->setCurrentIndex(3);
+}
+
+void MainWindow::on_pushButton_10_clicked() // boutton retour du widget modifier
+{
+    ui->stackedWidget->setCurrentIndex(2);
+}
+
+
+void MainWindow::on_supprimer_2_clicked() // boutton supprimer
+{
+    ui->stackedWidget->setCurrentIndex(4);
+}
+
+void MainWindow::on_pushButton_15_clicked() // boutton retour du widget supprimer
+{
+    ui->stackedWidget->setCurrentIndex(2);
+}
+
+void MainWindow::on_quitter_2_clicked() // retour du menu afficher
+{
+    ui->stackedWidget->setCurrentIndex(0);
+}
+
+void MainWindow::on_pushButton_3_clicked() // quitter l'application
+{QMessageBox::StandardButton reply = QMessageBox::question(this,"fenêtre","voulez vous quittez",QMessageBox::Yes| QMessageBox::No);
+
+    if(reply==QMessageBox::Yes){
+        QApplication::quit();
+    }else{
+        qDebug()<<"no is clicked";
+    }
+
+
+}
+
+
+
+void MainWindow::on_pushButton_16_clicked()// boutton ajouter
+{
+    QString id=ui->lineEdit_15->text();
+   QString nom=ui->lineEdit_16->text();
+          QString  prix=ui->lineEdit_17->text();
+           QString couleur=ui->lineEdit_18->text();
+            QString quantite=ui->lineEdit_19->text();
+            if(id==""||nom==""||prix==""||couleur==""||quantite==""){
+                   QMessageBox::warning(this,"ERROR","Veuillez remplir le champ requis");
+            }
+
+}
+
+
+void MainWindow::on_pushButton_17_clicked() // boutton undo (ajouter)
+{
+    ui->lineEdit_15->setText("");
+    ui->lineEdit_16->setText("");
+    ui->lineEdit_17->setText("");
+    ui->lineEdit_18->setText("");
+    ui->lineEdit_19->setText("");
+
+}
+
+
+void MainWindow::on_pushButton_18_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(0);
+}
+
+void MainWindow::on_pushButton_12_clicked() //boutton modifier (dans le widget modifier)
+{
+    QString emplacement=ui->lineEdit_12->text();
+   QString quantite=ui->lineEdit_10->text();
+          QString  quantite_dispo=ui->lineEdit_11->text();
+           QString prix=ui->lineEdit_13->text();
+
+            if(emplacement==""||quantite==""||prix==""||quantite_dispo==""){
+                   QMessageBox::warning(this,"ERROR","Veuillez remplir le champ requis");
+            }
+
+}
+
+
+
+void MainWindow::on_pushButton_11_clicked() // boutton undo (modifier)
+{
+    ui->lineEdit_12->setText("");
+    ui->lineEdit_10->setText("");
+    ui->lineEdit_11->setText("");
+    ui->lineEdit_13->setText("");
+
+}
+
+
+void MainWindow::on_pushButton_13_clicked() // boutton supprimer(widget supprimer)
+{
+   QString id_materiel= ui->lineEdit_14->text();
+   if(id_materiel==""){
+        QMessageBox::warning(this,"ERROR","Veuillez remplir le champ requis");
+   }
+}
+
+
+
+void MainWindow::on_pushButton_14_clicked() //boutton undo supprimer
+{
+
+ui->lineEdit_14->setText("");
+}
--- mainwindow.h
+++ mainwindow.h
@@ -1,7 +1,10 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-
+#include <QObject>
 #include <QMainWindow>
+#include<QPropertyAnimation>
+#include <QString>
+
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,12 +18,41 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+
 private slots:
     void on_pushButton_clicked();
 
-    void on_lineEdit_p_cursorPositionChanged(int arg1, int arg2);
-    
+    void on_pushButton_2_clicked();
+
+    void on_modifier_2_clicked();
+
+    void on_pushButton_10_clicked();
+
+    void on_supprimer_2_clicked();
+
+    void on_pushButton_15_clicked();
+
+    void on_quitter_2_clicked();
+
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_16_clicked();
+
+    void on_pushButton_17_clicked();
+
+    void on_pushButton_18_clicked();
+
+    void on_pushButton_12_clicked();
+
+    void on_pushButton_11_clicked();
+
+    void on_pushButton_13_clicked();
+
+    void on_pushButton_14_clicked();
+
 private:
     Ui::MainWindow *ui;
+      QPropertyAnimation *animation;
+      QPropertyAnimation *animation1;
 };
 #endif // MAINWINDOW_H
